e1e9408fa0a395542437885ff23decd21d74b632
Guang Ouyang <guang.ouyang@gmail.com>
2025-02-04 11:30:56 +0800

    Add files via upload

--- script_1.m
+++ script_1.m
@@ -2,9 +2,15 @@ clear;
 
 eeglab nogui;
 
+%change below to your own data path, e.g., 'D:\dataset1_face'
+data_path = 'your_data_path';
+%change below to your EEGLAB path
+eeglab_elec = 'your_EEGLAB_path\eeglab2022.0\plugins\dipfit\standard_BEM\elec\standard_1005.elc';
+data_name = 'face_001_1.vhdr';
+
 %load data, down_sample, and filter
-EEG = pop_loadbv('your_data_path\sample_data', 'face_001_1.vhdr', [], []);
-EEG = pop_chanedit(EEG, 'lookup','your_EEGLAB_path\eeglab2022.0\plugins\dipfit\standard_BEM\elec\standard_1005.elc');
+EEG = pop_loadbv(data_path, data_name, [], []);
+EEG = pop_chanedit(EEG, 'lookup',eeglab_elec);
 EEG = pop_resample(EEG, 250);
 EEG = pop_eegfiltnew(EEG,'hicutoff',40,'locutoff',1,'plotfreqz',0);
 
